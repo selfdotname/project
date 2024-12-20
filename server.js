1ae6fee32c44c6846ddd89be86eac68c36c8ccf5
Dietake Obaro <138878526+selfdotname@users.noreply.github.com>
2024-12-20 12:48:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const server = http.createServer((req, res) => {
   res.end('Hello');
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
   console.log('server is listening on port 3000');
 });
