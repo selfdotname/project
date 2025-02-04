fc8addd0d1702ad38a8546914a23bc1e56f0fd0c
Dietake Obaro <138878526+selfdotname@users.noreply.github.com>
2025-02-04 19:49:45 +0100

    Add files via upload

---
+++ Chat-Web-App/app.js
@@ -0,0 +1,31 @@
+const express       = require("express");
+const http          = require("http");
+const { Server }    = require("socket.io");
+
+const app           = express();
+const PORT          = process.env.PORT || 65535
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index", {port: PORT})
+})
+
+
+const server        = http.createServer(app);
+const io            = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message", (msg) => {
+    console.log("Message received:", msg);
+    socket.emit("message", "Message received!");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => console.log(`server listening on port ${PORT}`))
