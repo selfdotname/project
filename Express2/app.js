3eb00fe934bbe5bb1842da5212e40d9f5e8a0bdc
Dietake Obaro <138878526+selfdotname@users.noreply.github.com>
2025-02-06 21:31:41 +0100

    Add files via upload

---
+++ Express2/app.js
@@ -0,0 +1,14 @@
+const express           = require("express")
+const app               = express()
+
+const PORT              = process.env.PORT || 65_535
+
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+
+app.listen(PORT, () => console.log("Server is listening on port " + PORT))
+
+app.get("/", (req,  res) =>  {
+    res.render("index")
+})
