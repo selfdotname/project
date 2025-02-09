120573e1c95b9201e5c47c11a2c6860b596701fa
Dietake Obaro <138878526+selfdotname@users.noreply.github.com>
2025-02-09 08:46:48 +0100

    Update indexRoutes.js

--- Express8 Routes/routes/indexRoutes.js
+++ Express8 Routes/routes/indexRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads")
+        cb(null, "recieved")
     },
     filename: (req, file, cb) => {
         cb(null, new Date().getTime() + "-" + file.originalname)
